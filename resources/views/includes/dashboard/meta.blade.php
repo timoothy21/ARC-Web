9f636047b853216f8d49324094398ad7dbdd3bd1
DimasDaniZaini <dimasdanizaini123@gmail.com>
2023-05-25 19:50:21 +0700

    Slicing includes blades for dashboard

---
+++ resources/views/includes/dashboard/meta.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('asset/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('asset//favicon-16x16.png') }}">
+
+{{-- CSRF Token --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
