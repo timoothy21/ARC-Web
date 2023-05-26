b81e4bf444422e2a48ea4ecbecc1f510c76e3dc8
Muhammad Syauqi Frizman <muhammad.frizman@binus.ac.id>
2023-05-26 21:55:45 +0700

    update header dan footer

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.css('./resources/css/style-old.css', 'style.css')
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
